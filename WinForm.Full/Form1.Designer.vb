0366123886f2110081d8caa2d4e3b4c7ed335aa7
renanduart3 <renan110306@gmail.com>
2021-01-21 18:48:54 -0300

    texto 4 aprovado

--- WinForm.Full/Form1.Designer.vb
+++ WinForm.Full/Form1.Designer.vb
@@ -30,12 +30,12 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 55.0!)
-        Me.Label1.ForeColor = System.Drawing.Color.Sienna
+        Me.Label1.ForeColor = System.Drawing.Color.Chocolate
         Me.Label1.Location = New System.Drawing.Point(133, 167)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(714, 85)
+        Me.Label1.Size = New System.Drawing.Size(312, 85)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "GA - AutoDeploy 4.5"
+        Me.Label1.Text = "GA - 4.5"
         '
         'Label2
         '
