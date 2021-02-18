6f3630aa63d6772210fbc890236ed1198b0d8610
renanduart3 <renan110306@gmail.com>
2021-02-18 00:32:57 -0300

    duplicado o texto do app

--- WinForm.Full/Form1.Designer.vb
+++ WinForm.Full/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -46,11 +47,23 @@ Partial Class Form1
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Windows Forms VB .NET 4.5.2"
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 55.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.Sienna
+        Me.Label3.Location = New System.Drawing.Point(133, 82)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(714, 85)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "GA - AutoDeploy 4.5"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(897, 450)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -62,4 +75,5 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
